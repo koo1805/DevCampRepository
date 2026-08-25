f7e486b1831ebedc88fd2df00bd38b7e21876490
koo1805 <koo1805@naver.com>
2026-08-25 16:54:16 +0900

    엔진 재추가 - Sound Dll 추가 - Input 클래스 마우스 입력 추가

---
+++ 00CraftEngine/Game/Actor/TestActor.cpp
@@ -0,0 +1,61 @@
+﻿#include "TestActor.h"
+#include <Input/Input.h>
+#include <iostream>
+#include <Windows.h>
+
+using namespace Craft;
+TestActor::TestActor()
+	: Actor("P", Vector2(5, 5), Color::Green)
+{
+	// 그리기 순서 값 설정.
+	sortingOrder = 5;
+}
+
+void TestActor::Tick(float deltaTime)
+{
+	// 상위 로직 호출.
+	Actor::Tick(deltaTime);
+
+	// 프레임 관련 문자열.
+	const int size = 256;
+	char fpsString[size] = {};
+	sprintf_s(
+		fpsString, 
+		size, 
+		"dt: %f | fps: %.1f", 
+		deltaTime, 
+		(1.0f / deltaTime)
+	);
+
+	// 콘솔 창 이름에 값 설정.
+	SetConsoleTitleA(fpsString);
+
+	// ESC 키 종료.
+	if (Input::Get().GetKeyDown(VK_ESCAPE))
+	{
+		// 엔진 종료.
+		QuitGame();
+	}
+
+	// 방향키 이동 처리.
+	// @Temp: 프레임시간 고려는 나중에.
+	if (Input::Get().GetKey(VK_LEFT) && position.x > 0)
+	{
+		position.x -= 1;
+	}
+
+	if (Input::Get().GetKey(VK_RIGHT) && position.x < 39)
+	{
+		position.x += 1;
+	}
+
+	if (Input::Get().GetKey(VK_UP) && position.y > 0)
+	{
+		position.y -= 1;
+	}
+
+	if (Input::Get().GetKey(VK_DOWN) && position.y < 24)
+	{
+		position.y += 1;
+	}
+}
---
+++ 00CraftEngine/Game/Level/GameLevel.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include <Level/Level.h>
+#include <string>
+
+// 소코반 게임 레벨 클래스.
+// 게임 클리어 등 게임 규칙 및 전반을 관리.
+// 소코반 -> 박스를 정해진 위치에 모두 옮기는 것이 클리어 규칙.
+class GameLevel : public Craft::Level
+{
+	TYPE_DECLARATIONS(GameLevel, Level)
+
+public:
+	// 플레이어가 이동하려는 위치가 이동 가능한지를 판단해주는 함수.
+	bool CanMove(
+		const Craft::Vector2& playerPosition,
+		const Craft::Vector2& nextPosition
+	);
+
+private:
+	// 레벨 초기화 함수.
+	virtual void OnInitialized() override;
+
+	// Draw 이벤트 함수.
+	virtual void Draw() override;
+
+	// 맵 로드 함수.
+	void LoadMap(const std::string& filename);
+
+private:
+	// 점수 확인 함수.
+	bool CheckGameClear();
+
+private:
+	// 목표 점수 - 클리어 조건.
+	int targetScore = 0;
+
+	// 게임 클리어 여부 플래그.
+	bool isGameClear = false;
+};
