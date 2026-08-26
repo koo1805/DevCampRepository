ff4eb94b478ad92f564b480ae14162d3c39a9dd3
koo1805 <koo1805@naver.com>
2026-08-26 16:22:58 +0900

    정수/부동소수 난수

---
+++ 03Algorithm/Random/Main.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+// 범위를 지정해서 특정 범위의 정수 난수를 반환하는 함수
+// 나머지 연산 활용
+int RandomRange(int min, int max)
+{
+	const int diff = (max - min) + 1;
+	return (rand() % diff) + min;
+}
+
+// 나누기 연산을 활용한 방법
+// diff가 너무 크면 오버플로우
+int RandomRangeDivide(int min, int max)
+{
+	const int diff = (max - min) + 1;
+	return ((rand() * diff) / (RAND_MAX + 1)) + min;
+}
+
+// 부동소수 난수 생성 함수
+float RandomRange(float min, float max)
+{
+	// 0-1 사이의 부동소수 난수 구하기
+	float percent = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+
+	// 전달 받은 구간으로 변환
+	float diff = (max - min);
+	return (percent * diff) + min;
+}
+
+int main()
+{
+	// 현재 시간 값을 받음
+	// 종자값을 가변적으로 설정해야 함
+	// 많은 경우에 시간을 활용
+	int64_t seed = time(nullptr);
+
+	// 종자값(seed)
+	srand(static_cast<uint32_t>(seed));
+
+	const int count = 5;
+	for (int ix = 0; ix < count; ++ix)
+	{
+		//std::cout << "Random Num: " << rand() << "\n";
+		std::cout << "Random Num: " << RandomRange(1, 10) << "\n";
+	}
+}
