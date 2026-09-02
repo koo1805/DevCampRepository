226a9571c63e0f493485e08592b40ab4199766df
koo1805 <koo1805@naver.com>
2026-09-02 13:01:02 +0900

    구현 연습 문제 추가

---
+++ 01AditionalCPP/Test01/Main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+class Character
+{
+
+};
+
+class Warrior : public Character
+{
+
+};
+
+class Mage : public Character
+{
+
+};
+
+class Monster : public Character
+{
+
+};
+
+int main()
+{
+	
+
+}
