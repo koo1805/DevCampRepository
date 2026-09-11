9d8d186cdf83759ee06252b7dfe3a6bbadc6b9fa
koo1805 <koo1805@naver.com>
2026-09-11 14:49:05 +0900

    그래픽스 초기

---
+++ 04Grapics/CraftRender/Main.cpp
@@ -0,0 +1,19 @@
+#include <Windows.h>
+#include "LaunchApplication.h"
+
+#if _DEBUG
+int main()
+{
+    return LaunchApplication(GetModuleHandle(nullptr));
+}
+#else
+// 윈도우즈 모드에서 사용하는 메인 함수
+int WINAPI WinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPSTR lpCmdLine,
+    _In_ int nShowCmd)
+{
+    return LaunchApplication(hInstance);
+}
+#endif
